5601308f95622d197dd90acf83512f449cffdef3
Aniluv Villamor <aniluv.villamor@example.com>
2026-05-03 11:17:38 +0800

    chore: remove temporary helper scripts

--- fix_args.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let content = fs.readFileSync('apps/web/app/AdminSide/AdminAccounts.tsx', 'utf8');
-
-content = content.replace(/openSuspendModal\(acc\.id, acc\.name\)/g, "openSuspendModal(acc.id, acc.name, acc.status, acc.suspendedUntil)");
-content = content.replace(/openSuspendModal\(selectedAccount\.id, selectedAccount\.name\)/g, "openSuspendModal(selectedAccount.id, selectedAccount.name, selectedAccount.status, selectedAccount.suspendedUntil)");
-
-fs.writeFileSync('apps/web/app/AdminSide/AdminAccounts.tsx', content);
-console.log('Fixed args!');
